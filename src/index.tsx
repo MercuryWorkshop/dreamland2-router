3f8b54f41eb58eeb30f6a1853dd28f74f3578799
Percs <me@percs.dev>
2025-05-22 22:35:25 -0500

    start dreamland 2  migration

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
+import { scope, Component, ComponentInstance, DLElement } from "dreamland/core";
 let globalrouter: Router | null = null;
 
 export class Router {
   private el: HTMLElement = null!;
 
-  constructor(private root: ComponentElement<typeof Route>) {
+  constructor(private root: ComponentInstance<typeof Route>) {
     if (globalrouter) {
       throw new Error("Only one router can be created");
     }
@@ -19,7 +20,7 @@ export class Router {
   }
 
   public route(path: string): boolean {
-    if (this.root.$.path) throw new Error("Root route cannot have a path");
+    if (this.root.$.state.path) throw new Error("Root route cannot have a path");
 
     let url = new URL(path, location.origin);
 
@@ -31,9 +32,9 @@ export class Router {
   }
 
 
-  private subroute(path: string, subpath: string, root: ComponentElement<typeof Route>): boolean | null {
+  private subroute(path: string, subpath: string, root: ComponentInstance<typeof Route>): boolean | null {
     match: for (let route of root.$.children) {
-      let routepath = route.$.path;
+      let routepath = route.$.state.path;
       if (typeof routepath !== "string") throw new Error("Route must have a path");
       if (routepath[0] == "/") routepath = routepath.slice(1);
 
@@ -73,7 +74,7 @@ export class Router {
 
       if (route.$ instanceof Redirect) {
         let a = document.createElement("a");
-        let to = route.$.to;
+        let to = route.$.state.to;
         if (typeof to == "function") to = to(path, urlparams);
         a.href = to;
 
@@ -86,47 +87,47 @@ export class Router {
       if (route.$.children.length > 0) {
         // if child 404s start matching back from parent
 
-        let res = this.subroute(path, splitpath.join("/"), route as ComponentElement<typeof Route>);
+        let res = this.subroute(path, splitpath.join("/"), route as ComponentInstance<typeof Route>);
         if (res === null) return null;
 
         if (!res) continue match;
       }
 
       // if we got here, we have a match
-      let show = route.$.show;
+      let show = route.$.state.show;
       if (typeof show == "function") show = show(path, urlparams);
 
-      if (!show) throw new Error(`Route ${route.$.path} has no show target`);
+      if (!show) throw new Error(`Route ${route.$.state.path} has no show target`);
 
 
       if ("$" in show) {
         for (let key in urlparams) {
-          show.$[key] = urlparams[key];
+          show.$.state[key] = urlparams[key];
         }
 
-        show.$.routeshown = true;
-        if (show.$.routeshow)
-          show.$.routeshow(path);
+        show.$.state.routeshown = true;
+        if (show.$.state.routeshow)
+          show.$.state.routeshow(path);
       }
 
       for (let otherroute of root.$.children) {
         if (otherroute.$ instanceof Redirect) continue;
 
-        if (!otherroute.$.show) throw new Error(`Route ${otherroute.$.path} has no show target`);
-        if ("$" in otherroute.$.show && otherroute.$.show != show) {
-          otherroute.$.show.$.routeshown = false;
-          if (otherroute.$.show.$.routehide)
-            otherroute.$.show.$.routehide();
+        if (!otherroute.$.state.show) throw new Error(`Route ${otherroute.$.state.path} has no show target`);
+        if ("$" in otherroute.$.state.show && otherroute.$.state.show != show) {
+          otherroute.$.state.show.$.state.routeshown = false;
+          if (otherroute.$.state.show.$.state.routehide)
+            otherroute.$.state.show.$.state.routehide();
         }
       }
 
-      if (root == this.root && !this.root.$.show) {
+      if (root == this.root && !this.root.$.state.show) {
         this.el.replaceWith(show);
       } else {
-        let parentshow = root.$.show
+        let parentshow = root.$.state.show
         if (!("$" in parentshow!)) throw new Error("If subroutes are specified, show target must be a functional component");
 
-        parentshow.$.outlet = show;
+        parentshow.$.state.outlet = show;
       }
 
       return true;
@@ -136,9 +137,8 @@ export class Router {
   }
 
   public mount(root: HTMLElement) {
-
-    if (this.root.$.show) {
-      let show = this.root.$.show;
+    if (this.root.$.state.show) {
+      let show = this.root.$.state.show;
       if (typeof show == "function") show = show(location.pathname, {});
       this.el = show;
     } else {
@@ -151,10 +151,7 @@ export class Router {
     window.addEventListener("popstate", () => {
       this.route(location.pathname + location.search);
     });
-
   }
-
-
 }
 
 type ShowTarget = DLElement<{
@@ -169,9 +166,7 @@ type ShowTarget = DLElement<{
 export const Route: Component<{
   path?: string;
   show?: ShowTarget | ((path: string, params: Record<string, string>) => ShowTarget)
-}, {}, {
-  children: (ComponentElement<typeof Route> | ComponentElement<typeof Redirect>)[]
-}> = function() {
+}, {}, {}> = function() {
   // exists only to collect data
   return <div />;
 }
@@ -187,17 +182,11 @@ export const Redirect: Component<{
 export const Link: Component<{
   href: string;
   class?: string;
-}, {
-  _leak: true
-  root: HTMLAnchorElement
-  children: any
-}> = function() {
-  this._leak = true;
-
+}, {}> = function(cx) {
   return <a href={this.href} class={use(this.class)}
     on:click={e => {
       e.preventDefault();
       if (!globalrouter) throw new Error("No router exists");
-      globalrouter.navigate(this.root.href);
-    }}>{this.children}</a>
+      globalrouter.navigate((cx.root as HTMLAnchorElement).href);
+    }}>{cx.children}</a>
 }
